4f079a0f60669d56feb2f9cef66dcbc870ddfced
Julian Ramos <julianc.ramos21@gmail.com>
2024-10-24 18:02:36 -0500

    Implementación de propiedad para llevar a un end point específico después del login

--- src/main/java/org/example/practicaspringsecurity/config/SecurityConfig.java
+++ src/main/java/org/example/practicaspringsecurity/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -40,31 +41,39 @@ public class SecurityConfig {
 //    }
 
     //Configuration 2
-//    @Bean
-//    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
-//        return httpSecurity
-//                .authorizeHttpRequests( auth -> {
-//                    auth.requestMatchers("/v1/index2").permitAll();
-//                })
-//                .formLogin().permitAll()
-//                .and()
-//                .build();
-//    }
-
-    //Para version 6.1
     @Bean
     public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .authorizeHttpRequests(auth -> auth
-                        // Permitir acceso sin autenticación a la ruta específica
-                        .requestMatchers("/v1/index2").permitAll()
-                        // Indicar que cualquier otra ruta requiere autenticación
-                        .anyRequest().authenticated()
-                )
-                .formLogin(withDefaults())  // Configuración predeterminada para el formulario de login
+                .authorizeHttpRequests( auth -> {
+                    auth.requestMatchers("/v1/index2").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin()
+                    .successHandler(successHandler())
+                    .permitAll()
+                .and()
                 .build();
     }
 
+    //Para version 6.1
+//    @Bean
+//    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+//        return httpSecurity
+//                .authorizeHttpRequests(auth -> auth
+//                        // Permitir acceso sin autenticación a la ruta específica
+//                        .requestMatchers("/v1/index2").permitAll()
+//                        // Indicar que cualquier otra ruta requiere autenticación
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin(withDefaults())  // Configuración predeterminada para el formulario de login
+//                .build();
+//    }
+
+    public AuthenticationSuccessHandler successHandler(){
+        return ((request, response, auth) -> {
+            response.sendRedirect("/v1/index");
+        });
+    }
 
 
 }
