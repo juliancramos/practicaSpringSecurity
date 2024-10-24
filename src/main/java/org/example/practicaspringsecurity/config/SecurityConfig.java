2ac662e11133fb71463477506871fdd0902feafd
Julian Ramos <julianc.ramos21@gmail.com>
2024-10-24 17:45:14 -0500

    Implementación de security filter chain para version 6.1

--- src/main/java/org/example/practicaspringsecurity/config/SecurityConfig.java
+++ src/main/java/org/example/practicaspringsecurity/config/SecurityConfig.java
@@ -1,23 +1,42 @@
 package org.example.practicaspringsecurity.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
 
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        return httpSecurity
+//                .authorizeHttpRequests()
+//                    .requestMatchers("/v1/index2").permitAll()
+//                    .anyRequest().authenticated()
+//                .and()
+//                .formLogin().permitAll()
+//                .and().
+//                build();
+//
+//
+//    }
+
+    //Para version de SpringBoot 6
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .authorizeHttpRequests()
-                    .requestMatchers("/v1/index2").permitAll()
-                    .anyRequest().authenticated()
-                .and()
-                .formLogin().permitAll()
-                .and().
-                build();
-
-
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/v1/index2").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .permitAll()
+                )
+                .build();
     }
+
+
+
 }
 
