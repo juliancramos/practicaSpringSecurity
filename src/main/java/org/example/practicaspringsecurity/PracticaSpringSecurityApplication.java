c54cbf67a3268fbbe38dc649f725280c4c39b1ae
Julian Ramos <julianc.ramos21@gmail.com>
2024-10-24 17:02:40 -0500

    Commit inicial

---
+++ src/main/java/org/example/practicaspringsecurity/PracticaSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.practicaspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/practicaspringsecurity/PracticaSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.practicaspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
