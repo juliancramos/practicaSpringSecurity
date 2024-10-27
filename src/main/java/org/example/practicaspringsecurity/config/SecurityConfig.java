ec8bb5d03378e3194e8fd42ac621c7752935172e
Julian Ramos <julianc.ramos21@gmail.com>
2024-10-27 18:08:16 -0500

    Se implementan las sesiones

--- src/main/java/org/example/practicaspringsecurity/config/SecurityConfig.java
+++ src/main/java/org/example/practicaspringsecurity/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package org.example.practicaspringsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -49,8 +52,21 @@ public class SecurityConfig {
                     auth.anyRequest().authenticated();
                 })
                 .formLogin()
-                    .successHandler(successHandler())
+                    .successHandler(successHandler())//Url a donde irá después de iniciar sesión (mirar successHandler)
                     .permitAll()
+                .and()
+                .sessionManagement()
+                    //Política de creación de sesión. Always crea sesión siempre que no exista, if solo si se necesita,
+                    //Never no crea, pero si ya existe la utiliza. Stateless no trabaja con sesiones
+                    .sessionCreationPolicy(SessionCreationPolicy.ALWAYS) //Always -If_Required - never - stateless
+                    .invalidSessionUrl("/login") //url invalido se va a login
+                    .maximumSessions(1) //Sirve para cuando se pueden tener varias sesiones
+                    .expiredUrl("/login") //End point para cuando se acaba la sesión
+                    .sessionRegistry(sessionRegistry()) //Definir un objeto que se encarga de administrar los registros de la sesión
+                .and()
+                .sessionFixation()
+                    //Si se detecta un ataque de fijación de sesión, Spring genera otro id de sesión
+                    .migrateSession() //migrateSession - newSession - none .
                 .and()
                 .build();
     }
@@ -68,6 +84,10 @@ public class SecurityConfig {
 //                .formLogin(withDefaults())  // Configuración predeterminada para el formulario de login
 //                .build();
 //    }
+    @Bean
+    public SessionRegistry sessionRegistry(){
+        return new SessionRegistryImpl(); //Se habilita este objeto para el rastro de los datos del usuario
+    }
 
     public AuthenticationSuccessHandler successHandler(){
         return ((request, response, auth) -> {
